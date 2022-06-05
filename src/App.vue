4c782d78e11d0e38cf1d7684ecfccf112679b5e3
crisboxing11 <crisboxing11@gmail.com>
2022-06-05 16:18:54 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Hello you Mofo</h1>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+/* eslint-disable prettier/prettier */
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./assets/tailwind.css";
+
+createApp(App).mount("#app");
