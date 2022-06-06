7a15784e10f544c35905da3d0a959e07ac6927b9
crisboxing11 <crisboxing11@gmail.com>
2022-06-05 21:49:50 -0700

    Created Header

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,10 +1,11 @@
 <template>
   <header class="w-full text-sm">
-    <div class="fixed top-0 left-0 w-full h-16 bg-white"></div>
-    <div
-      class="flex flex-nowrap h-full px-8 mx-auto border-b border-solid border-brand-blue-1"
-    >
-      <a href="/">Red Code Arrow</a>
+    <div class="fixed top-0 left-0 w-full h-16 bg-white">
+      <div
+        class="flex flex-nowrap h-full px-8 mx-auto border-b border-solid border-brand-blue-1"
+      >
+        <a href="/" class="flex items-center h-full text-xl">Red Code Arrow</a>
+      </div>
     </div>
   </header>
 </template>
