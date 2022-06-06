e8d65c5895eca2429073a7444077bd579acfd620
crisboxing11 <crisboxing11@gmail.com>
2022-06-05 17:25:53 -0700

    Fixed config issues

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>Hello you Mofo</h1>
+  <h1 class="mt-8 text-6xl text-center">Hello you Mofo</h1>
 </template>
 
 <script>
---
+++ src/components/MainNav.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1 class="w-full text-sm">Main Naver man</h1>
+</template>
