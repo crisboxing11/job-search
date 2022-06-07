d014595aec4f05bf204ef90a30e607943f3d5ee3
crisboxing11 <crisboxing11@gmail.com>
2022-06-06 20:42:46 -0700

    Added Testing

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,9 +2,14 @@
   <header class="w-full text-sm">
     <div class="fixed top-0 left-0 w-full h-16 bg-white">
       <div
-        class="flex flex-nowrap h-full px-8 mx-auto border-b border-solid border-brand-blue-1"
+        class="flex flex-nowrap h-full px-8 mx-auto border-b border-solid border-brand-gray-1"
       >
-        <a href="/" class="flex items-center h-full text-xl">Red Code Arrow</a>
+        <a href="/" class="flex items-center h-full text-3xl font-semibold">{{
+          company
+        }}</a>
+        <h2 class="flex items-center h-full ml-8 font-semibold">
+          Developed by: {{ author }}
+        </h2>
       </div>
     </div>
   </header>
@@ -13,5 +18,11 @@
 <script>
 export default {
   name: "MainNav",
+  data() {
+    return {
+      company: "RedCode Jobs",
+      author: "Cristian Arroyo Colon",
+    };
+  },
 };
 </script>
---
+++ tests/unit/playground.test.js
@@ -0,0 +1,9 @@
+describe("basic math", () => {
+  it("adds two numbers", () => {
+    expect(1 + 1).toBe(2);
+  });
+
+  it("substracts two numbers", () => {
+    expect(2 - 1).toBe(1);
+  });
+});
