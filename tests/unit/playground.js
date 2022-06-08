db0ae4efcb9e5fe1f301193aa5e55797ad9f1681
crisboxing11 <crisboxing11@gmail.com>
2022-06-08 12:14:15 -0700

    added additional tests

---
+++ tests/unit/playground.js
@@ -0,0 +1,5 @@
+export const evenOrOdd = (number) => {
+  return number % 2 === 0 ? "even" : "odd";
+};
+
+export const multiply = (a, b) => a * b;
--- tests/unit/playground.test.js
+++ tests/unit/playground.test.js
@@ -1,3 +1,5 @@
+import { evenOrOdd, multiply } from "./playground";
+
 describe("basic math", () => {
   it("adds two numbers", () => {
     expect(1 + 1).toBe(2);
@@ -5,5 +7,20 @@ describe("basic math", () => {
 
   it("substracts two numbers", () => {
     expect(2 - 1).toBe(1);
+  });
+});
+
+describe("evenOrOdd", () => {
+  it("return even numbers", () => {
+    expect(evenOrOdd(2)).toBe("even");
+  });
+  it("returns an odd number", () => {
+    expect(evenOrOdd(1)).toBe("odd");
+  });
+});
+
+describe("multiply", () => {
+  it("Multiplies two numbers together", () => {
+    expect(multiply(2, 2)).toBe(4);
   });
 });
